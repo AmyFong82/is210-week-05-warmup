ca877f2784ad323d690cd283ad6600f45083d707
Amy Fong <AmyhFong@gmail.com>
2016-09-22 03:21:53 +0000

    Submitting warmup 3 tasks.

---
+++ task_01/peanut.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Print Truthy or Falsy."""
+
+BUTTER = True
+OIL = False
---
+++ task_03.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Copy BUTTER from peanut."""
+
+from task_01.peanut import BUTTER
+
+JELLY = BUTTER
